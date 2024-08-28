9fdf23e59c7e7fb53e5cb5f9fbe2f8c92042da90
jabz-untouched <israel.ekebafe@gmail.com>
2024-08-28 15:00:13 +0100

    create the about page

---
+++ app/about/page.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='max-w-6xl mx-auto p-3'>
+        <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+        
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi sed, reiciendis similique facere voluptatibus, odit veritatis sint placeat amet voluptatem deleniti sequi, accusamus debitis vero incidunt temporibus veniam corporis deserunt.
+        Alias accusamus temporibus voluptatibus facere quae eveniet libero officia odio. Quo omnis esse facilis veniam, tempora et id neque nulla illo? Dignissimos harum corrupti nemo, illo quasi perferendis magni ducimus?
+        Facere accusantium, odio id suscipit fugit, natus nihil dolorum quasi corrupti eius officiis iure ullam culpa sint sequi. Excepturi totam ex saepe, vel ipsa delectus odio nostrum? Voluptates, tenetur doloremque!</p>
+<br />
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi sed, reiciendis similique facere voluptatibus, odit veritatis sint placeat amet voluptatem deleniti sequi, accusamus debitis vero incidunt temporibus veniam corporis deserunt.
+        Alias accusamus temporibus voluptatibus facere quae eveniet libero officia odio. Quo omnis esse facilis veniam, tempora et id neque nulla illo? Dignissimos harum corrupti nemo, illo quasi perferendis magni ducimus?
+        Facere accusantium, odio id suscipit fugit, natus nihil dolorum quasi corrupti eius officiis iure ullam culpa sint sequi. Excepturi totam ex saepe, vel ipsa delectus odio nostrum? Voluptates, tenetur doloremque!</p>
+<br />
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi sed, reiciendis similique facere voluptatibus, odit veritatis sint placeat amet voluptatem deleniti sequi, accusamus debitis vero incidunt temporibus veniam corporis deserunt.
+        Alias accusamus temporibus voluptatibus facere quae eveniet libero officia odio. Quo omnis esse facilis veniam, tempora et id neque nulla illo? Dignissimos harum corrupti nemo, illo quasi perferendis magni ducimus?
+        Facere accusantium, odio id suscipit fugit, natus nihil dolorum quasi corrupti eius officiis iure ullam culpa sint sequi. Excepturi totam ex saepe, vel ipsa delectus odio nostrum? Voluptates, tenetur doloremque!</p>
+        </div>
+  )
+}
