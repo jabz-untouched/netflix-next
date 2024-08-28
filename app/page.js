c735c29a436d4db1e35cfcd5cf262447707f44e7
jabz-untouched <israel.ekebafe@gmail.com>
2024-08-28 17:23:58 +0100

    fetch data from tmdb api in home page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,27 @@
+const API_KEY =process.env.API_KEY;
 import Image from "next/image";
+import Results from "./components/Results";
 
-export default function Home() {
+export default async function Home({searchParams}) {
+  const genre = searchParams.genre || 'fetchTrending';
+  const res= await fetch(
+    `https://api.themoviedb.org/3${
+      genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
+    }?api_key=${API_KEY}&language=en-US&page=1`
+  );
+
+  const data = await res.json ();
+  if (!res.ok){
+    throw new Error('Failed to fetch data');
+  }
+
+  const results = data.results;
+  console.log(results.original_title);
+
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>Home</div>
+      <Results results={results} />
     </main>
   );
 }
