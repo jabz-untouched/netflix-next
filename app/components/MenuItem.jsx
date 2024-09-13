2f447d7d0b113653d32d182628e9f456976f3734
jabz-untouched <israel.ekebafe@gmail.com>
2024-09-13 21:56:02 +0100

    MenuItemcomp apply suspense boundary

--- app/components/MenuItem.jsx
+++ app/components/MenuItem.jsx
@@ -1,12 +1,15 @@
+// MenuItem.tsx
+
+import React, { Suspense } from "react";
 import Link from "next/link";
-import React from "react";
 
 function MenuItem({ title, address, Icon }) {
   return (
     <Link href={address} className="hover:text-amber-500">
-        <Icon className='text-2xl sm:hidden' />
-        <p className=" uppercase hidden sm:inline text-sm"> {title} </p>
-    
+      <Suspense fallback={<div>Loading...</div>}>
+        <Icon className="text-2xl sm:hidden" />
+        <p className="uppercase hidden sm:inline text-sm">{title}</p>
+      </Suspense>
     </Link>
   );
 }
