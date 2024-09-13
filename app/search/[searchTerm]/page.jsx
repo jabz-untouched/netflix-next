11b0b539ff11f171ed1ceedf0b31b591f8afa893
jabz-untouched <israel.ekebafe@gmail.com>
2024-09-13 22:24:01 +0100

    fixed error again

--- app/search/[searchTerm]/page.jsx
+++ app/search/[searchTerm]/page.jsx
@@ -1,27 +1,20 @@
-// Searchpage.tsx
-
-import React, { Suspense } from "react";
 import Results from "@/app/components/Results";
+import React from "react";
 
-export default function Searchpage({ params }) {
+export default async function Searchpage({ params }) {
   const searchTerm = params.searchTerm;
-  const res = fetch(
+  const res = await fetch(
     `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${searchTerm}&language=en-US&page=1&include_adult=false`
   );
 
-  // Wrap the fetch call with Suspense
-  const data = res.json();
+  const data = await res.json();
   const results = data.results;
-
   return (
     <div>
-      {results && results.length === 0 ? (
-        <h1 className="text-center pt-6">No results found</h1>
-      ) : (
-        <Suspense fallback={<div>Loading...</div>}>
-          <Results results={results} />
-        </Suspense>
-      )}
+      {results &&
+        results.length ===
+        <h1 className=" text-center pt-6 ">No results found</h1>}
+      {results && <Results results={results} />}
     </div>
   );
 }
