764d822315035b8ce778801c75fddfe4b85c1667
jabz-untouched <israel.ekebafe@gmail.com>
2024-09-13 22:06:46 +0100

    page apply suspense boundary

--- app/search/[searchTerm]/page.jsx
+++ app/search/[searchTerm]/page.jsx
@@ -1,20 +1,27 @@
-import Results from "@/app/components/Results";
-import React from "react";
+// Searchpage.tsx
 
-export default async function Searchpage({ params }) {
+import React, { Suspense } from "react";
+import Results from "@/app/components/Results";
+
+export default function Searchpage({ params }) {
   const searchTerm = params.searchTerm;
-  const res = await fetch(
+  const res = fetch(
     `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${searchTerm}&language=en-US&page=1&include_adult=false`
   );
 
-  const data = await res.json();
+  // Wrap the fetch call with Suspense
+  const data = res.json();
   const results = data.results;
+
   return (
     <div>
-      {results &&
-        results.length ===
-        <h1 className=" text-center pt-6 ">No results found</h1>}
-      {results && <Results results={results} />}
+      {results && results.length === 0 ? (
+        <h1 className="text-center pt-6">No results found</h1>
+      ) : (
+        <Suspense fallback={<div>Loading...</div>}>
+          <Results results={results} />
+        </Suspense>
+      )}
     </div>
   );
 }
