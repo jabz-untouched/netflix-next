29c0e6b071db42bc1addf7e1f554e8e3ef9deed2
jabz-untouched <israel.ekebafe@gmail.com>
2024-09-13 21:57:22 +0100

    NavBarcomp apply suspense boundary

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -1,12 +1,16 @@
-import React from "react";
+// NavBar.tsx
+
+import React, { Suspense } from "react";
 import NavBarItem from "./NavBarItem";
 
 export default function NavBar() {
   return (
     <div className="w-screen h-11 bg-amber-100 py-6 my-2 flex items-center space-x-7 mx-auto text-slate-950">
-      <div className=" flex lg: text-lg mx-auto space-x-7 font-extrabold">
-        <NavBarItem title="Trending" param="fetchTrending" />
-        <NavBarItem title="Top Rated" param="fetchTopRated" />
+      <div className="flex lg:text-lg mx-auto space-x-7 font-extrabold">
+        <Suspense fallback={<div>Loading...</div>}>
+          <NavBarItem title="Trending" param="fetchTrending" />
+          <NavBarItem title="Top Rated" param="fetchTopRated" />
+        </Suspense>
       </div>
     </div>
   );
