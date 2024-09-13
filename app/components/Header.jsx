5ad0841d2dd45c664ee78ef291993ceaa8f47905
jabz-untouched <israel.ekebafe@gmail.com>
2024-09-13 21:54:11 +0100

    header.jsx comp apply suspense boundary

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,24 +1,30 @@
-import React from "react";
+// Header.tsx
+
+import React, { Suspense } from "react";
 import MenuItem from "./MenuItem";
-import {AiFillHome} from 'react-icons/ai';
-import {BsFillInfoCircleFill} from 'react-icons/bs'
+import { AiFillHome } from "react-icons/ai";
+import { BsFillInfoCircleFill } from "react-icons/bs";
 import Link from "next/link";
 import DarkModeSwitch from "./DarkModeSwitch";
 
 const Header = () => {
   return (
-    <div className=" flex justify-between items-center p-7 max-w-6xl mx-auto">
+    <div className="flex justify-between items-center p-7 max-w-6xl mx-auto">
       <div className="flex gap-4">
         <MenuItem title="home" address="/" Icon={AiFillHome} />
         <MenuItem title="about" address="/about" Icon={BsFillInfoCircleFill} />
       </div>
 
-      <div className=" flex gap-1 items-center">
-        <DarkModeSwitch />
-        <Link href= '/' >
-        <span className="text-2xl font-bold bg-amber-600 hover:bg-amber-500 py-1 px-2 rounded-lg">Netflow</span>
+      <div className="flex gap-1 items-center">
+        <Suspense fallback={<div>Loading...</div>}>
+          <DarkModeSwitch />
+        </Suspense>
+        <Link href="/">
+          <span className="text-2xl font-bold bg-amber-600 hover:bg-amber-500 py-1 px-2 rounded-lg">
+            Netflow
+          </span>
         </Link>
-        <span className="text-xl hidden sm:inline" >Web</span>
+        <span className="text-xl hidden sm:inline">Web</span>
       </div>
     </div>
   );
