59bbef44597142dff00bbe87aa966ae8601773ed
jabz-untouched <israel.ekebafe@gmail.com>
2024-09-13 22:13:05 +0100

    main page apply suspense boundary

--- app/page.js
+++ app/page.js
@@ -1,28 +1,28 @@
 const API_KEY =process.env.API_KEY;
-import Image from "next/image";
+
+// Home.tsx
+
+import React, { Suspense } from "react";
 import Results from "./components/Results";
 
-export default async function Home({searchParams}) {
-  const genre = searchParams.genre || 'fetchTrending';
-  const res= await fetch(
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+  const res = fetch(
     `https://api.themoviedb.org/3${
-      genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
+      genre === "fetchTopRated" ? "/movie/top_rated" : "/trending/all/week"
     }?api_key=${API_KEY}&language=en-US&page=1`,
-    {next: {revalidate: 10000}}
+    { next: { revalidate: 10000 } }
   );
 
-  const data = await res.json ();
-  if (!res.ok){
-    throw new Error('Failed to fetch data');
-  }
-
+  // Wrap the fetch call with Suspense
+  const data = res.json();
   const results = data.results;
-  console.log(results.original_title);
-
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Results results={results} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Results results={results} />
+      </Suspense>
     </main>
   );
 }
